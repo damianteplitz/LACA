eb578c9cf40a21403dde86ed5fb21a0b6e5aca16
Damian <dteplitz@menhir.com.ar>
2019-03-26 16:41:05 -0300

    Footer en el fondo siempre

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,15 @@
-<em>&copy; Designed by Damian Teplitz & Co.</em>
-        </body>
+<nav class="navbar navbar-fixed-bottom" id="footer">
+  <em>&copy; Designed by Damian Teplitz & Co.</em>
+  </div>
+</nav>
+
+
+
+</body>
 </html>
+
+<script>
+let footer = document.getElementById("footer");
+footer.style.bottom = "0px";  
+footer.style.position = "fixed"; 
+</script>
