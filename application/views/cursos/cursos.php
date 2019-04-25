37c1229320a1c90f966b941ad7d6274d57542c42
Damian <dteplitz@menhir.com.ar>
2019-04-25 17:02:47 -0300

    Titulo Curso/Taller

--- application/views/cursos/cursos.php
+++ application/views/cursos/cursos.php
@@ -8,7 +8,7 @@
                 <div class="modal-content">
                 <!-- Modal Header -->
                     <div class="modal-header">
-                        <h4 class="modal-title">Curso</h4>
+                        <h4 class="modal-title">Curso/Taller</h4>
                         <button type="button" class="close" data-dismiss="modal">&times;</button>
                     </div>
                     <!-- Modal body -->
